7d1bf0bdcb9276bda0f219eec07f5a289001a3a3
nanakhaytee123@gmail.com <nanakhaytee123@gmail.com>
2023-08-16 14:42:49 +0000

    my first commit :tada:

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+floatvariable = 10.5
+integervariable = "Carlos"
+stringvariable =23
+booleanvariable = True 
+print(floatvariable)
+print(stringvariable)
+print(booleanvariable)
+print(integervariable)
